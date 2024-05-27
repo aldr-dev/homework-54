9225c75f5b58ceacb8491aee3759822a27a47048
Gordus Alexander <aldrmiller@gmail.com>
2024-05-27 19:41:45 +0600

    реализовал компонент доска

---
+++ src/components/Board/Board.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Items} from '../../types';
+import Cell from '../Cell/Cell';
+
+interface BoardProps {
+  items: Items[];
+  onClickCell: (index: number) => void;
+}
+
+const Board: React.FC<BoardProps> = ({items, onClickCell}) => {
+  return (
+    <div className="board">
+      {items.map((cell, index) => (
+        <Cell key={index} item={cell} onClick={() => onClickCell(index)} />
+      ))}
+    </div>
+  );
+};
+
+export default Board;
